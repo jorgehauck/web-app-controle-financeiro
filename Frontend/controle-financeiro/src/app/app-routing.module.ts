04b5382b138c5108193bdb34a51469e605c1f2f9
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2023-03-22 22:12:41 -0300

    feature: Adição de guarda de rotas

--- Frontend/controle-financeiro/src/app/app-routing.module.ts
+++ Frontend/controle-financeiro/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacaoGuard } from './guard/autenticacao.guard';
+import { LoginGuard } from './guard/login.guard';
 
 const routes: Routes = [
   {
@@ -9,11 +11,13 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren:() => import('./pages/home/home.module').then((h) => h.HomeModule)
+    loadChildren:() => import('./pages/home/home.module').then((h) => h.HomeModule),
+    canLoad: [LoginGuard]
   },
   {
     path: 'receitas',
-    loadChildren: () => import('./pages/lista-receitas/lista-receitas.module').then((l) => l.ListaReceitasModule)
+    loadChildren:() => import('./pages/lista-receitas/lista-receitas.module').then((l) => l.ListaReceitasModule),
+    canLoad: [AutenticacaoGuard]
   }
 ];
 
--- Frontend/controle-financeiro/src/app/components/cabecalho/cabecalho.component.ts
+++ Frontend/controle-financeiro/src/app/components/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cabecalho',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabecalho.component.css']
 })
 export class CabecalhoComponent implements OnInit {
+
+
+  @Input()
+  public nomeUsuarioLogado: string = '';
 
   constructor() { }
 
